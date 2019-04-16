6c5581e7185ac98e223f9e4fa660b9b3c2f90e55
Minh Phan <minh.phan@qwconsult.com>
2019-04-16 13:15:06 -0700

    Initial Setup For personal repo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Max', age: '28'}, 
+      { name: 'Manu', age: '29'}, 
+      { name: 'Setphanie', age: '26' }      
+    ], 
+    otherState: 'some other value'
+  })
+
+  const switchNameHandler = () => {
+    console.log('Was Clicked!')
+    // DON'T DO THIS: personsState.persons[0].name = "Maximillian"
+    setPersonsState({
+      persons: [
+        { name: 'Max', age: '28'}, 
+        { name: 'Manu', age: '29'}, 
+        { name: 'Stephanie', age: '27' }      
+      ],  
+    })
+  }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Hi, I'm a React App</h1>
+       <button onClick={switchNameHandler}>Switch Name</button>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
-  }
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app'))
 }
 
-export default App;
+
+export default app;
+
