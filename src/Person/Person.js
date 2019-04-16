7c7d4a95eefd7d6b64e3f9f00d138542abdf96a4
Minh Phan <mpflamingmarshmallow@gmail.com>
2019-04-16 14:19:16 -0700

    Basic features and syntax

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Person.css'
 
 
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I'm a {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I'm a {props.age} years old!</p>
             <p>{props.children}</p>
+            <input onChange={props.changed} type="text" value={props.name}></input>
         </div>
         
     )
